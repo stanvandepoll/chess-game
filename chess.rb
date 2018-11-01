78e0643e7b80f09c17575503c81434c79f7f23b4
Stan van de Poll <swvandepoll@gmail.com>
2018-11-01 22:10:46 +0100

    make place_piece method, rewrite knight-board communication and start display and knight placement test

--- chess.rb
+++ chess.rb
@@ -1,37 +1,37 @@
 class Knight
   attr_reader :position, :team
-  def initialize(start_position, team)
-    @position = start_position
+  def initialize(position, team, boardname)
+    @position = position
     @team = team
-    chessboard.place_piece(@position, @team, knight)
+    boardname.place_piece(@position, @team, :knight)
   end
 
-  def move(start, destination)
+  def move(start, destination, board)
     unless move_allowed?(start, destination)
       puts "That move is not allowed by a knight."  
       return nil
     end
     if start != @position
       puts "Your knight is not on that position, so we can't move it."
-    elsif chessboard.matrix[destination] != " "
-      if chessboard.matrix[destination].teamcolor?(@team) 
+    elsif board.matrix[destination] != " "
+      if board.matrix[destination].teamcolor?(@team) 
         puts "One of your pieces is already on the destination tile."
       else 
         puts "You capture one of your opponents pieces!"
-        chessboard.capture_piece(destination)
+        board.capture_piece(destination)
         @position = destination
-        chessboard.remove_piece(start)
-        chessboard.place_piece(@position, @team, knight)
+        board.remove_piece(start)
+        board.place_piece(@position, @team, knight)
       end
     else
       @position = destination
-      chessboard.remove_piece(start)
-      chessboard.place_piece(@position, @team, knight)
+      board.remove_piece(start)
+      board.place_piece(@position, @team, knight)
     end
   end
 
   def move_allowed?(start, destination)
-
+    true
   end
 
 end
@@ -39,7 +39,7 @@ end
 ###############################################################################
 
 class Board
-  attr_reader :matrix
+  attr_accessor :matrix
   def initialize
     @matrix = Array.new(8){ Array.new(8, " ") }
   end
@@ -53,9 +53,9 @@ class Board
   end
 
   def place_piece(position, team, type)
-    # not sure whether this works
-    # use a hash for both teams that give the correct string for the type
-    @matrix[position] = string
+    symbol_hash = {king: "K", queen: "Q", rook: "R", bishop: "B", knight: "N", pawn: "p"}
+    symbol = symbol_hash[type]
+    @matrix[position[0]][position[1]] = symbol
   end
 
   def remove_piece(position)
@@ -75,5 +75,9 @@ class Game
   end
 end
 
-chessboard = Board.new
-chessboard.display()
+board = Board.new
+board.display()
+knight = Knight.new([7,1], "black", board)
+board.display()
+# knight.move([7,1], [5,2], board)
+# knight.move([5,2], [1,1], board)
